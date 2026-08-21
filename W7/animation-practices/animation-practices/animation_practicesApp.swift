88d9600332a5d2a19663d4d10525a6565453d745
TaiVanNgo <vantaingo.056@gmail.com>
2026-08-21 11:59:00 +0700

    creat week7 project

---
+++ W7/animation-practices/animation-practices/animation_practicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  animation_practicesApp.swift
+//  animation-practices
+//
+//  Created by Van Tai on 21/8/26.
+//
+
+import SwiftUI
+
+@main
+struct animation_practicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
